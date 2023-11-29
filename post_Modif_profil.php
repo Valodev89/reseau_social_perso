cabc722e9831a7a4deaccef3467026ec7c782163
Valentin <onraet.valentin@outlook.fr>
2023-11-29 20:19:49 +0100

    mise à jour

--- post_Modif_profil.php
+++ post_Modif_profil.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 
 session_start();  
-
 include('includes/config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -13,13 +12,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // Requête SQL pour mettre à jour le nom de l'utilisateur dans la base de données
     $requete = $db->prepare("UPDATE utilisateurs SET name = :newname, lastname = :newlastname, birthday = :newbirthday, mail = :newmail, password = :newpassword WHERE id = :id");
-    $requete->bindParam(':newname', $newname);
-    $requete->bindParam(':newlastname', $newlastname);
-    $requete->bindParam(':newbirthday', $newbirthday);
-    $requete->bindParam(':newmail', $newmail);
-    $requete->bindParam(':newpassword', $newpassword);
-    $requete->bindParam(':id', $id); // Assurez-vous d'avoir l'ID de l'utilisateur
-    $requete->execute();
+    $requete->execute([
+        'lastname' => $newlastname,
+        'name' => $newname,
+        'birthday' => $newbirthday,
+        'mail' => $newmail,
+        'password' => $newpassword
+    ]);
     
     // Redirection vers la page de profil ou autre traitement
     header("Location: Page_profil.php");
